2b736e9af79aed8be4195ac069404ecc888934f4
razorenstein <133526610+razorenstein@users.noreply.github.com>
2024-09-06 11:55:50 +0300

    user-id validation middleware is now on the shared lib

--- apps/wallet-service/src/wallet-service.module.ts
+++ apps/wallet-service/src/wallet-service.module.ts
@@ -3,11 +3,10 @@ import { WalletServiceController } from './controllers/wallet-service.controller
 import { WalletAssetServiceController } from './controllers/wallet-asset-service.controller';
 import { LoggingModule } from '@shared/logging';
 import { FileManagementModule } from '@shared/file-management';
-import { UserIdValidationMiddleware } from './middlewares/user-id-validation.middleware';
 import { HttpModule } from '@nestjs/axios';
 import { UserAssetsController } from './controllers/user-assets.controller';
 import { SupportedValuesController } from './controllers/supported-values.controller';
-import { IsAssetIdSupportedConstraint } from '@shared/utils/validation/asset-id-validator.decorator';
+import { IsAssetIdSupportedConstraint, UserIdValidationMiddleware } from '@shared/utils/validation';
 import { RateService } from './services/rate-service-api.service';
 import { SupportedValuesService } from './services/supported-values.service';
 import { UserAssetsService } from './services/user-assets.service';
